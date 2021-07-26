8355a54cf0f2da7762c4fae65ed4fdc21d23b0dd
fbarrueta22 <frings-22@hotmail.com>
2021-07-26 16:30:31 -0500

    finish

--- final-software/parcialsoftware/src/main/java/main/CentroVacunacion.java
+++ final-software/parcialsoftware/src/main/java/main/CentroVacunacion.java
@@ -5,14 +5,12 @@ import java.util.List;
 
 public class CentroVacunacion {
     private boolean isBaja;
-    private List<String> personasVacunadas;
     private String distrit;
     private int vacunasParciales;
     private int vacunasCompletas;
 
     public CentroVacunacion(String distrito, int vacunasParciales, int vacunasCompletas) {
         isBaja = false;
-        personasVacunadas = new ArrayList<>();
         this.distrit = distrito;
         this.vacunasParciales = vacunasParciales;
         this.vacunasCompletas = vacunasCompletas;
